bf55612e6d92ab16a5003b76a2801687f2aee7a1
miwanoff <marina.iva.noffa@gmail.com>
2025-11-29 12:45:28 +0200

    Add wrap.style.width

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const treasure = {
   x: 100,
   y: 200,
 };
+
+const info = document.getElementById("info");
+const map = document.getElementById("map");
+const coords = document.getElementById("coords");
+const wrap = document.getElementById("wrap");
+
+// map.width = 400;
+
+console.log(map);
+console.log(map.width);
+console.log(map.height);
+
+wrap.style.width = map.width + "px";
+
+map.onmousemove = function (event) {
+//   console.log(event);
+};
